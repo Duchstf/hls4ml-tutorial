b3a7de1113f66f019bf069d42f9cdabd55dd05b3
Duchstf <minhduc8199@gmail.com>
2020-06-08 13:11:55 -0500

    add generated source

---
+++ hls4ml_prj_3/firmware/weights/b11.h
@@ -0,0 +1,15 @@
+//Numpy array shape [5]
+//Min -0.116954371333
+//Max 0.111225351691
+//Number of zeros 0
+
+#ifndef B11_H_
+#define B11_H_
+
+#ifndef __SYNTHESIS__
+model_default_t b11[5];
+#else
+model_default_t b11[5] = {-0.1169543713, -0.0057334346, 0.1112253517, 0.0496534593, 0.0818614364};
+#endif
+
+#endif
